97a942f18af34558e53c0ecdb5bcc993a2746ede
Z-iutek <ziutugra@gmail.com>
2024-05-26 17:54:26 +0200

    Edycja calego tf i local moze zadziala

--- main.tf
+++ main.tf
@@ -1,18 +1,9 @@
-provider "aws" {
-  alias      = "localstack"
-  access_key = "mock_access_key"
-  secret_key = "mock_secret_key"
-  region     = "us-east-1"
-  skip_credentials_validation = true
-  skip_requesting_account_id  = true
-  skip_metadata_api_check     = true
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = var.bucket_name
 
-  endpoints {
-    s3 = "http://localhost:4566"
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
   }
 }
-
-resource "aws_s3_bucket" "example" {
-  provider = aws.localstack
-  bucket   = var.bucket_name
-}
+#test
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,3 @@
 output "bucket_name" {
-  description = "The name of the S3 bucket"
-  value       = aws_s3_bucket.example.bucket
+  value = aws_s3_bucket.my_bucket.bucket
 }
