8f37b2fe615af739b2672fd972a1571d4bc7a110
Z-iutek <ziutugra@gmail.com>
2024-05-26 16:46:25 +0200

    Zmiany które moze naprawia Pipeline na githubie

--- main.tf
+++ main.tf
@@ -2,7 +2,11 @@ provider "aws" {
   access_key = "mock_access_key"
   secret_key = "mock_secret_key"
   region     = "us-east-1"
-  endpoint   = "http://localhost:4566"  # Localstack endpoint
+
+  s3 {
+    endpoint = "http://localhost:4566"
+    force_path_style = true
+  }
 }
 
 resource "aws_s3_bucket" "example" {
